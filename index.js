097f967e0f4fb0f7eba144af741d70de5b579c02
Zeynab Hamad <zaynabhamad950@gmail.com>
2022-12-06 00:10:09 +0300

    compleated

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+
+// 1.
+document.getElementById("main").remove();
+
+// 2.
+const newHeader= document.createElement("h1");
+ 
+//3.
+newHeader.setAttribute("id", "victory");
+
+//4.
+newHeader.innerHTML= "Zeynab is the champion";
